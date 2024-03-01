42b05868f22dd3024bb4f5db0878fd0d29560425
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-03-01 18:28:37 -0300

    refactor: images

--- src/components/Company/Comp.tsx
+++ src/components/Company/Comp.tsx
@@ -13,7 +13,7 @@ const Comp: React.FC = () => {
       <Marquee direction="right" gradient={true} delay={1} speed={50} gradientColor="#8B0000, #080F26" gradientWidth={10} >
         <div className="flex flex-col items-center">
           <img 
-            src="https://cdn.discordapp.com/attachments/1205228548986511420/1207817571852881920/logo-steaks-branco.png?ex=65e106c5&is=65ce91c5&hm=a543031046172e05d037889ade753fbdb0493d0eb5edd9f2304a854055a47ef4&" 
+            src="https://cdn.discordapp.com/attachments/1205228548986511420/1207817571852881920/logo-steaks-branco.png?ex=65f37bc5&is=65e106c5&hm=3bea62c970b6785bac0df180cc51088e3cc6b2daf8a30646b08da975d1343494&" 
             alt=""
             className="w-[30%] h-[30%]"
           />
@@ -21,7 +21,7 @@ const Comp: React.FC = () => {
         </div>
         <div className="flex flex-col items-center">
           <img 
-            src="https://cdn.discordapp.com/attachments/1205228548986511420/1207817572196548670/zarla-santo-steak-1x1-2400x2400-20220324-hqbb3d7bvrc46brbvyht.png?ex=65e106c5&is=65ce91c5&hm=f69fcd58d266bbea56a99c84c42cfe19fc9c144c7d8ea8002d40b4c0fc93d9ea&" 
+            src="https://cdn.discordapp.com/attachments/1205228548986511420/1207817572196548670/zarla-santo-steak-1x1-2400x2400-20220324-hqbb3d7bvrc46brbvyht.png?ex=65f37bc5&is=65e106c5&hm=4ebd9840c89d8a24e854f72195336385020717152cb7e9d9c7ad1d7ce13d860d&" 
             alt=""
             className="w-[30%] h-[30%]"
           />
@@ -29,7 +29,7 @@ const Comp: React.FC = () => {
         </div>
         <div className="flex flex-col items-center">
           <img 
-            src="https://cdn.discordapp.com/attachments/1205228548986511420/1207817572431691886/casa_do_churrasco_logomarca_fundo_cinz.png?ex=65e106c5&is=65ce91c5&hm=466866d37e9a5f0a7b9a6aac1c920277a0587ad5805747576a8eb2d0065b637e&" 
+            src="https://cdn.discordapp.com/attachments/1205228548986511420/1207817572431691886/casa_do_churrasco_logomarca_fundo_cinz.png?ex=65f37bc5&is=65e106c5&hm=a8818a42a0bea92f032a195bdd2debac6751a1b39f5784d5195fd24afaea7d87&" 
             alt=""
             className="w-[30%] h-[30%]"
           />
@@ -37,7 +37,7 @@ const Comp: React.FC = () => {
         </div>
          <div className="flex flex-col items-center">
           <img 
-            src="https://cdn.discordapp.com/attachments/1205228548986511420/1207817864237817886/907993.png?ex=65e1070b&is=65ce920b&hm=9eb520a53e07af89a906f48199d1fdd4e376490cc9e14b499647808b773f5219&" 
+            src="https://cdn.discordapp.com/attachments/1205228548986511420/1207817864237817886/907993.png?ex=65f37c0b&is=65e1070b&hm=b6063c317611d8f74b9fd147a38bf27e4942c521be2c26b44b035da65439c07c&" 
             alt=""
             className="w-[30%] h-[30%]"
           />
@@ -45,7 +45,7 @@ const Comp: React.FC = () => {
         </div>
         <div className="flex flex-col items-center">
           <img 
-            src="https://cdn.discordapp.com/attachments/1205228548986511420/1207817864493535253/logo_big_churrasco.png?ex=65e1070b&is=65ce920b&hm=dbbdc5a7ab1c0299f2492b57c7d8fcc827d6ec055b9bf2e28c8359f59cc4c4c7&" 
+            src="https://cdn.discordapp.com/attachments/1205228548986511420/1207817864493535253/logo_big_churrasco.png?ex=65f37c0b&is=65e1070b&hm=a9e142d2e6b431580272396f0e5d233beefa19e3b3e81b5fbdb08b468f7c4eaf&" 
             alt=""
             className="w-[30%] h-[30%]"
           />
@@ -53,7 +53,7 @@ const Comp: React.FC = () => {
         </div>
         <div className="flex flex-col items-center">
           <img 
-            src="https://cdn.discordapp.com/attachments/1205228548986511420/1207818174121119925/101166.png?ex=65e10754&is=65ce9254&hm=63a6240cbc56cefcb144e348729a48036081ff8add70453144d4cd34d11d8ecb&" 
+            src="https://cdn.discordapp.com/attachments/1205228548986511420/1207818174121119925/101166.png?ex=65f37c54&is=65e10754&hm=29a57622f353b0cc39ab85de2c0f1e08e30f05502a2b3239df41227d2f7b6c39&" 
             alt=""
             className="w-[30%] h-[30%]"
           />
