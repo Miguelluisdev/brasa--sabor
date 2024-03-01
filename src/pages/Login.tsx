7101a4943081ccddae817e7e954d94fafe202585
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-03-01 18:52:05 -0300

    feat: :sparkles: login component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,13 @@
+import LoginComponent from "../components/Login/LoginComponent"
 
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div  style={{backgroundImage: "url('https://cdn.discordapp.com/attachments/1205228548986511420/1211426287311134780/proximo-do-delicioso-churrasco-mexicano.jpg?ex=65ee27a6&is=65dbb2a6&hm=4eb71d0e41ee6031444394646926180bc8ca8616bd9c163bb07464b983717655&')" ,  backgroundBlendMode: 'darken', backgroundColor: 'rgba(0, 0, 0, 0.412)' }} >
+      <LoginComponent/>
+    </div>
   )
+  
 }
 
 export default Login
